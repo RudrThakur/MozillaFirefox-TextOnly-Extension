0dd4523d32a7607be4dad0aa3f090c31fc473de0
RudrThakur <rudrakshacmkt777@gmail.com>
2017-03-19 10:17:24 +0530

    Add files via upload

---
+++ content_script.js
@@ -0,0 +1,15 @@
+var element = document.querySelector(".banner");
+element.parentElement.removeChild(element);
+
+//var element1 = document.querySelector(".shop_canvas_td");
+//element1.parentElement.removeChild(element1);
+
+
+
+
+var elementone = document.getElementsByTagName("img"), index;
+
+for (index = elementone.length - 1; index >= 0; index--) {
+    elementone[index].parentNode.removeChild(elementone[index]);
+}
+
